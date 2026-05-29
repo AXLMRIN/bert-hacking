e293a28b9efd57e1430ab3b6f510769329750a22
A.Morin <axelmorinmail@gmail.com>
2026-05-29 10:12:01 +0200

    time the run

--- src/single_run.py
+++ src/single_run.py
@@ -50,12 +50,14 @@ def single_run(
     """
 
     logger = CustomLogger("./custom_logs")
+    run_timer = {}
 
     # Use time as hash
     hash_, logs_to_save = create_hash(loop_config), None
     tokenizer, ds_loop, dsd_loop, ds_pred, predictions, model = (None,) * 6
     try: 
         # Dichotomization: dichotomization_label
+        run_timer["preprocess_data"] = time()
         dichotomized_df, label2id, id2label = dichotomize(df, loop_config)
         dichotomized_df_prediction, _, _ = dichotomize(df_prediction, loop_config)
         
@@ -77,7 +79,10 @@ def single_run(
         dsd_loop : DatasetDict = split_ds(ds_loop, loop_config)
         dsd_loop = dsd_loop.map(lambda row: tokenize_dataset_dict(row,label2id, tokenizer,tokenization_parameters))
         
+        run_timer["preprocess_data"] = time() - run_timer["preprocess_data"] 
+        
         # Prepare model: model_name
+        run_timer["training"] = time()
         model = AutoModelForSequenceClassification.from_pretrained(
             loop_config.model_name,
             num_labels = len(label2id),
@@ -94,8 +99,10 @@ def single_run(
         tstart = time()
         best_model_checkpoint = train_model(model, training_args,dsd_loop,loop_config)
         logger(f"Training done in {time() - tstart:.0f}s - best model checkpoint: {best_model_checkpoint}")
+        run_timer["training"] = time() - run_timer["training"] 
         
         # Reload model from checkpoint: test_mode, device_batch_size
+        run_timer["evaluation"] = time()
         model = AutoModelForSequenceClassification.from_pretrained(best_model_checkpoint)
         predictions : pd.DataFrame = predict(model, dsd_loop["test"], loop_config, id2label=id2label)
         predictions_aggregated : pd.DataFrame = aggregate_predictions(predictions, label2id, id2label, THRESHOLD, AT_LEAST)
@@ -106,8 +113,10 @@ def single_run(
             zero_division=np.nan
         )
         logger(f"Evaluate best model. Score: {score_on_test}")
+        run_timer["evaluation"] = time() - run_timer["evaluation"]
 
         # Predict on full data
+        run_timer["prediction"] = time() 
         ds_pred = Dataset.from_pandas(dichotomized_df_prediction)
         ds_pred = ds_pred.map(lambda row: tokenize_dataset_dict(row,label2id, tokenizer,tokenization_parameters))
 
@@ -115,23 +124,28 @@ def single_run(
         tstart = time()
         predictions : pd.DataFrame = predict(model, ds_pred, loop_config, id2label=id2label)
         logger(f"Inference done in {time() - tstart:.0f} s")
+        run_timer["prediction"] = time() - run_timer["prediction"]
 
         if not loop_config.test_mode:
+            run_timer["saving_predictions"] = time()
             predictions.to_csv(f"./predictions_save/{hash_}.csv")
+            run_timer["saving_predictions"] = time() - run_timer["saving_predictions"]
             logs_to_save = {
                 **loop_config.to_dict(),
                 "effective_context_window": max_length_capped,
                 "score_on_test": score_on_test,
                 "prediction-csv": f"./predictions_save/{hash_}.csv",
-                "effective_distrib": effective_distrib
+                "effective_distrib": effective_distrib,
             }                
             if "ID_CHUNK" in predictions.columns:
+                run_timer["saving_predictions_aggregated"] = time()
                 (
                     aggregate_predictions(predictions, label2id, id2label, THRESHOLD, AT_LEAST)
                     .to_csv(f"./predictions_save/{hash_}-aggregated.csv")
                 )
                 logs_to_save["prediction-aggregated-csv"] = f"./predictions_save/{hash_}-aggregated.csv"
-
+                run_timer["saving_predictions_aggregated"] = time() - run_timer["saving_predictions_aggregated"] 
+            logs_to_save["run_timer"] = run_timer
             logger(f"Information saved with hash {hash_}")
             
     except Exception as e: 
